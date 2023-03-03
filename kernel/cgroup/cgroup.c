d2730e83b1ffdfa905a4b8b00aba573bfbbe3e8b
Suren Baghdasaryan <surenb@google.com>
2023-03-02 17:13:46 -0800

    psi: remove 500ms min window size limitation for triggers
    Current 500ms min window size for psi triggers limits polling interval
    to 50ms to prevent polling threads from using too much cpu bandwidth by
    polling too frequently. However the number of cgroups with triggers is
    unlimited, so this protection can be defeated by creating multiple
    cgroups with psi triggers (triggers in each cgroup are served by a single
    "psimon" kernel thread).
    Instead of limiting min polling period, which also limits the latency of
    psi events, it's better to limit psi trigger creation to authorized users
    only, like we do for system-wide psi triggers (/proc/pressure/* files can
    be written only by processes with CAP_SYS_RESOURCE capability). This also
    makes access rules for cgroup psi files consistent with system-wide ones.
    Add a CAP_SYS_RESOURCE capability check for cgroup psi file writers and
    remove the psi window min size limitation.
    
    Suggested-by: Sudarshan Rajagopalan <quic_sudaraja@quicinc.com>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lore.kernel.org/all/cover.1676067791.git.quic_sudaraja@quicinc.com/

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -3803,6 +3803,14 @@ static __poll_t cgroup_pressure_poll(struct kernfs_open_file *of,
 	return psi_trigger_poll(&ctx->psi.trigger, of->file, pt);
 }
 
+static int cgroup_pressure_open(struct kernfs_open_file *of)
+{
+	if (of->file->f_mode & FMODE_WRITE && !capable(CAP_SYS_RESOURCE))
+		return -EPERM;
+
+	return 0;
+}
+
 static void cgroup_pressure_release(struct kernfs_open_file *of)
 {
 	struct cgroup_file_ctx *ctx = of->priv;
@@ -5104,6 +5112,7 @@ static struct cftype cgroup_base_files[] = {
 	{
 		.name = "io.pressure",
 		.flags = CFTYPE_PRESSURE,
+		.open = cgroup_pressure_open,
 		.seq_show = cgroup_io_pressure_show,
 		.write = cgroup_io_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -5112,6 +5121,7 @@ static struct cftype cgroup_base_files[] = {
 	{
 		.name = "memory.pressure",
 		.flags = CFTYPE_PRESSURE,
+		.open = cgroup_pressure_open,
 		.seq_show = cgroup_memory_pressure_show,
 		.write = cgroup_memory_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -5120,6 +5130,7 @@ static struct cftype cgroup_base_files[] = {
 	{
 		.name = "cpu.pressure",
 		.flags = CFTYPE_PRESSURE,
+		.open = cgroup_pressure_open,
 		.seq_show = cgroup_cpu_pressure_show,
 		.write = cgroup_cpu_pressure_write,
 		.poll = cgroup_pressure_poll,
